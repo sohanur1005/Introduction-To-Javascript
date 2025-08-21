b8d7c57cad1932214ef56d25f4addad419645c6b
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 02:42:22 +0600

    send this

---
+++ naming.js
@@ -0,0 +1,36 @@
+/**
+ * 1.No keyword in variable name
+
+var false=45;
+var var=40;
+
+ 2.No space or gap
+var first name="sohanur"
+
+3.No quote
+var "name"="sohanur"
+
+4.can not start with a number
+var 2money=20;
+
+5.case sensitive .thats means every name as a seperate value
+ address
+ Address
+ ADDRESS
+
+ 
+
+ */
+// 6.Long variable  name
+var my current address="Bagha,Rajshahi"
+// Snake case
+var my_current_address="Bagha,Rajshahi"; 
+// camel case
+var myCurrentAddress="Bagha,Rajshahi"
+// pascal case
+var MyCurrentAddress="Bagha,Rajshahi"; 
+
+
+
+
+
---
+++ shortand.js
@@ -0,0 +1,4 @@
+var price=20;
+// price=price+10;
+price+=10;
+console.log(price)
