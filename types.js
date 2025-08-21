4f15de269c9e6ac64975feeee7c0631b6987c67a
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-21 14:01:20 +0600

    send

--- types.js
+++ types.js
@@ -1,3 +1,9 @@
+// number type
+var price=500;
+console.log(price)
+console.log(typeof(price))
+
+
 // string type variable
 
 var name="sohanur rahman"
@@ -7,3 +13,11 @@ var country='Bangladesh'
 console.log(name)
 console.log(address)
 console.log(country)
+console.log(typeof(country))
+
+
+// boolean type
+var isRich=true;
+var passed=false;
+console.log(isRich)
+console.log(typeof(passed))
