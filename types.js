1e5a9b9bcc378dce2fc73a70cf311c39fdc3cd81
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-21 13:50:17 +0600

    send this

---
+++ types.js
@@ -0,0 +1,9 @@
+// string type variable
+
+var name="sohanur rahman"
+var address="Rajshahi"
+var country='Bangladesh'
+
+console.log(name)
+console.log(address)
+console.log(country)
---
+++ variable.js
@@ -0,0 +1,16 @@
+/**
+ * 5 things to declare a variable
+ * 
+ * 
+ */
+var weight=60;
+var price=1000;
+var year=2002;
+
+// change value
+var money=200;
+money=500;
+
+console.log(weight)
+console.log(year)
+console.log(money)
