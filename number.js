5cb4084140ce636d8a06bfe338cd9131124fa52d
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 02:27:17 +0600

    send this

--- number.js
+++ number.js
@@ -1,6 +1,17 @@
 var orangePrice=20;
 var applePrice='20';
+var apple=parseInt('20');
+var num=parseInt('Twenty')
 
 console.log(typeof(orangePrice))
 console.log(typeof( applePrice))
 console.log(orangePrice +  applePrice)
+console.log(orangePrice +  apple)
+console.log(num)
+
+
+var first=0.1;
+var second=0.2;
+
+var total=first+second;
+console.log(total.toFixed(2))
